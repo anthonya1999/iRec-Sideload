545c52856bccdff4d999fcc94f61f0548966d99f
Anthony Agatiello <anthonya1999@gmail.com>
2015-12-14 18:58:38 -0500

    Add project

---
+++ iRec-Sideload/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  iRec-Sideload
+//
+//  Created by Anthony Agatiello on 12/14/15.
+//  Copyright © 2015 Anthony Agatiello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, nil);
+    }
+}
